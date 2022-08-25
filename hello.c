1e3cb7f09a022ea0a463077541ead0a7d031f8a2
jdcake <jasondibraccio@gmail.com>
2022-08-25 20:05:31 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jason DiBraccio
+ *         jdibraccio2@huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Computer Science\n");
+
+  return 0;
+}
